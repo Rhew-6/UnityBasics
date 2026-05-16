4f191a3e24b003c77a92bff56d4aa9fd26ced701
Rhew-6 <lidiomiguely646@gmail.com>
2026-05-16 16:28:01 -0400

    Add files via upload

---
+++ playerMovementG.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerMovement : MonoBehaviour
+{
+    Vector2 vec = Vector2.zero;
+    float speed = 10;
+    void Update()
+    {
+        if (Keyboard.current.aKey.isPressed && Keyboard.current.dKey.isPressed)
+        {
+            vec.x = 0;
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            vec.x = -1;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            vec.x = 1;
+        }
+        else
+        {
+            vec.x = 0;
+        }
+
+        if (Keyboard.current.wKey.isPressed && Keyboard.current.sKey.isPressed)
+        {
+            vec.y = 0;
+        }
+        else if (Keyboard.current.wKey.isPressed)
+        {
+            vec.y = 1;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            vec.y = -1;
+        }
+        else
+        {
+            vec.y = 0;
+        }
+
+        vec = vec.normalized;
+        transform.position += new Vector3(vec.x, vec.y, 0) * speed * Time.deltaTime;
+    }
+}
